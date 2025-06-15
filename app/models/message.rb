d94cf08fffcab39d9a104b7da7d837c1e9e6c95f
Fábio de Melo <104263483+FabioMelo10@users.noreply.github.com>
2025-06-15 20:48:15 -0300

    Add read flag to messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,10 @@
 class Message < ApplicationRecord
   belongs_to :conversation
   belongs_to :user
+
+  attribute :read, :boolean, default: false
+
+  def mark_as_read!
+    update(read: true)
+  end
 end
--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "new messages are unread by default" do
+    conversation = conversations(:one)
+    user = users(:one)
+    message = Message.new(conversation: conversation, user: user, body: "Hello")
+    assert_not message.read
+    message.save!
+    assert_not message.reload.read
+  end
+
+  test "mark_as_read! updates read flag" do
+    message = messages(:one)
+    assert_not message.read
+    message.mark_as_read!
+    assert message.reload.read
+  end
 end
